feef87ce37f7eb6e634ed6c73e1899b35f1ecb28
Miceuz <mic@hardcore.lt>
2012-09-07 11:16:49 +0300

    +firmware, thicker traces, etc

---
+++ src/main.c
@@ -0,0 +1,165 @@
+#include <inttypes.h>
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <avr/wdt.h>
+#include <avr/sleep.h>
+
+void inline initBuzzer() {
+//    PRR &= ~_BV(PRTIM1);
+    TCCR1A = 0; //reset timer1 configuration
+    TCCR1B = 0;
+
+    TCCR1A |= _BV(COM1A1);  //set OC1A to low on compare match
+    TCCR1A |= _BV(WGM10);   //PWM, Phase Correct, 8-bit 
+    TCCR1B |= _BV(CS10);    //start timer, no prescaling
+//    PRR |= _BV(PRTIM1);
+}
+
+void beep() {
+    initBuzzer();
+    OCR1A = 128;
+    _delay_ms(20);
+    TCCR1B = 0;    //stop timer
+}
+
+void  chirp(uint8_t times) {
+    while (times-- > 0) {
+        beep();
+        _delay_ms(20);
+    }
+}
+
+void inline initComparator() {
+    PRR &= ~_BV(PRADC);                 //start ADC
+    ACSR |= _BV(ACIS0) | _BV(ACIS1);    //Comparator Interrupt on Rising Output Edge.
+    ACSR &= ~_BV(ACIE);                 //disable comparator interrupt
+    ACSR &= ~_BV(ACD);                  //enable comparator
+    ACSR |= _BV(ACIE);                  //enable comparator interrupt
+}
+
+volatile uint16_t chargeTime = 0;
+volatile uint8_t resultReady = 0;
+
+ISR(ANA_COMP_vect) {
+    chargeTime = TCNT1;
+    ACSR &= ~_BV(ACIE); //disable comparator interrupt as comparator goes crazy when we discharge capacitor
+    resultReady = 1;
+}
+
+void measureCapacitance() {
+    chargeTime = 0;
+    
+    PORTA &= ~_BV(PA0);     //discharge capacitor
+    _delay_ms(1);           //one milisecond should be enough?
+    
+//    PRR &= ~_BV(PRTIM1);
+    TCCR1B = 0;
+    TCNT1 = 0;              //set timer to 0
+    TCCR1A = 0;             //reset timer1 configuration
+
+    TCCR1B = _BV(CS10);    //start timer, no prescaling
+    PORTA |= _BV(PA0);      //start charging capacitor
+
+    initComparator();
+        
+    resultReady = 0;
+    while(!resultReady) {
+        //NOTHING 
+    }
+    ACSR |= _BV(ACD);       //disable comparator
+    PORTA &= ~_BV(PA0);     //discharge capacitor
+    TCCR1B = 0;             //stop timer
+//    PRR |= _BV(PRTIM1);
+    PRR |= _BV(PRADC);      //shutdown ADC
+}
+
+void inline spiTransfer16(uint16_t data) {
+    PRR &= ~_BV(PRUSI);
+    USIDR = data >> 8;
+    uint8_t counter = 8;
+    PORTA &= ~ _BV(PA3);
+    while(counter-- > 0) {
+        USICR = _BV(USIWM0) | _BV(USITC);
+        USICR = _BV(USIWM0) | _BV(USITC) | _BV(USICLK);
+    }
+    USIDR = data;
+    counter = 8;
+    while(counter-- > 0) {
+        USICR = _BV(USIWM0) | _BV(USITC);
+        USICR = _BV(USIWM0) | _BV(USITC) | _BV(USICLK);
+    }
+    PORTA |= _BV(PA3);
+    PRR |= _BV(PRUSI);
+}
+
+ISR(WATCHDOG_vect ) {
+    beep();
+}
+
+void inline initWatchdog() {
+    WDTCSR |= _BV(WDCE); 
+    WDTCSR &= ~_BV(WDE);
+    WDTCSR |= _BV(WDIE); 
+    WDTCSR |= _BV(WDP3) | _BV(WDP0); 
+}
+
+void inline setupGPIO() {
+    PORTA |= _BV(PA0);  //V_CHARGE
+    PORTA &= ~_BV(PA0);                     
+    DDRA |= _BV(PA3) | _BV(PA4) | _BV(PA5); //USI interface
+    PORTA |= _BV(PA3);  //set USI CS to high
+    DDRA |= _BV(PA6);   //piezo buzzer
+    PORTA &= ~_BV(PA6);
+    DDRA |= _BV(PA7);   //nothing 
+    PORTA &= ~_BV(PA7);
+    
+    DDRB |= _BV(PB0);   //nothing
+    PORTB &= ~_BV(PB0);
+    DDRB |= _BV(PB1);   //nothing
+    PORTB &= ~_BV(PB1);
+}
+
+void inline setupPowerSaving() {
+    DIDR0 |= _BV(ADC1D) | _BV(ADC2D);   //disable digital input buffer on AIN0 and AIN1
+    ADCSRA &= ~_BV(ADEN);               //disable ADC
+    PRR |= _BV(PRTIM0);                 //disable timer0
+}
+
+void inline sleep() {
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+    sleep_enable();
+    MCUCR |= _BV(BODS) | _BV(BODSE);    //disable brownout detection during sleep
+    MCUCR &=~ _BV(BODSE);
+    sleep_cpu();
+    sleep_disable();
+}
+
+int main (void) {
+    setupPowerSaving();
+    setupGPIO();
+    sei();
+    
+    uint16_t referenceChargeTime = 0;
+    _delay_ms(1000);
+    
+    uint8_t t = 10;
+    while(t-- > 0){
+        measureCapacitance();
+        if(chargeTime > referenceChargeTime) {
+            referenceChargeTime = chargeTime;
+        }
+    }
+
+    chirp(2);
+    initWatchdog();
+
+    while(1){
+        measureCapacitance();
+        if(chargeTime > referenceChargeTime + 5) {
+            chirp(3);
+        }
+        //spiTransfer16(chargeTime);
+        sleep();
+    }
+}
