bcd7aeb5d01c7b8bbe9b60af765ee462a9a852c7
miceuz <mic@hardcore.lt>
2018-03-27 22:58:36 +0300

    Add soft uart library for debug purposes

---
+++ src/dbg_putchar.c
@@ -0,0 +1,87 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42, by Joerg Wunsch):
+ * <dinuxbg .at. gmail.com> wrote this file.  As long as you retain this notice
+ * you can do whatever you want with this stuff. If we meet some day, and you 
+ * think this stuff is worth it, you can buy me a beer in return.
+ *                                                             Dimitar Dimitrov
+ * ----------------------------------------------------------------------------
+ */
+
+#include <stdint.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#include "dbg_putchar.h"
+
+#if DBG_UART_ENABLE
+
+void dbg_putchar(uint8_t c)
+{
+#define DBG_UART_TX_NUM_DELAY_CYCLES	((F_CPU/DBG_UART_BAUDRATE-16)/4+1)
+#define DBG_UART_TX_NUM_ADD_NOP		((F_CPU/DBG_UART_BAUDRATE-16)%4)
+	uint8_t sreg;
+	uint16_t tmp;
+	uint8_t numiter = 10;
+
+	sreg = SREG;
+	cli();
+
+	asm volatile (
+		/* put the START bit */
+		"in %A0, %3"		"\n\t"	/* 1 */
+		"cbr %A0, %4"		"\n\t"	/* 1 */
+		"out %3, %A0"		"\n\t"	/* 1 */
+		/* compensate for the delay induced by the loop for the
+		 * other bits */
+		"nop"			"\n\t"	/* 1 */
+		"nop"			"\n\t"	/* 1 */
+		"nop"			"\n\t"	/* 1 */
+		"nop"			"\n\t"	/* 1 */
+		"nop"			"\n\t"	/* 1 */
+
+		/* delay */
+	   "1:" "ldi %A0, lo8(%5)"	"\n\t"	/* 1 */
+		"ldi %B0, hi8(%5)"	"\n\t"	/* 1 */
+	   "2:" "sbiw %A0, 1"		"\n\t"	/* 2 */
+		"brne 2b"		"\n\t"	/* 1 if EQ, 2 if NEQ */
+#if DBG_UART_TX_NUM_ADD_NOP > 0
+		"nop"			"\n\t"	/* 1 */
+  #if DBG_UART_TX_NUM_ADD_NOP > 1
+		"nop"			"\n\t"	/* 1 */
+    #if DBG_UART_TX_NUM_ADD_NOP > 2
+		"nop"			"\n\t"	/* 1 */
+    #endif
+  #endif
+#endif
+		/* put data or stop bit */
+		"in %A0, %3"		"\n\t"	/* 1 */
+		"sbrc %1, 0"		"\n\t"	/* 1 if false,2 otherwise */
+		"sbr %A0, %4"		"\n\t"	/* 1 */
+		"sbrs %1, 0"		"\n\t"	/* 1 if false,2 otherwise */
+		"cbr %A0, %4"		"\n\t"	/* 1 */
+		"out %3, %A0"		"\n\t"	/* 1 */
+
+		/* shift data, putting a stop bit at the empty location */
+		"sec"			"\n\t"	/* 1 */
+		"ror %1"		"\n\t"	/* 1 */
+
+		/* loop 10 times */
+		"dec %2"		"\n\t"	/* 1 */
+		"brne 1b"		"\n\t"	/* 1 if EQ, 2 if NEQ */
+		: "=&w" (tmp),			/* scratch register */
+		  "=r" (c),			/* we modify the data byte */
+		  "=r" (numiter)		/* we modify number of iter.*/
+		: "I" (_SFR_IO_ADDR(DBG_UART_TX_PORT)),
+		  "M" (1<<DBG_UART_TX_PIN),
+		  "i" (DBG_UART_TX_NUM_DELAY_CYCLES),
+		  "1" (c),			/* data */
+		  "2" (numiter)
+	);
+	SREG = sreg;
+}
+#undef DBG_UART_TX_NUM_DELAY_CYCLES
+#undef DBG_UART_TX_NUM_ADD_NOP
+
+#endif
+
---
+++ src/dbg_putchar.h
@@ -0,0 +1,60 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42, by Joerg Wunsch):
+ * <dinuxbg .at. gmail.com> wrote this file.  As long as you retain this notice
+ * you can do whatever you want with this stuff. If we meet some day, and you 
+ * think this stuff is worth it, you can buy me a beer in return.
+ *                                                             Dimitar Dimitrov
+ * ----------------------------------------------------------------------------
+ */
+
+#ifndef DBG_PUTCHAR_H
+#define DBG_PUTCHAR_H
+
+#include <stdint.h>
+
+/* User setting -> Whether to enable the software UART */
+#define DBG_UART_ENABLE		1
+
+/* User setting -> Output pin the software UART */
+#define DBG_UART_TX_PORT	PORTA
+#define DBG_UART_TX_DDR		DDRA
+#define DBG_UART_TX_PIN		PA4
+
+/* User setting -> Software UART baudrate. */
+#define DBG_UART_BAUDRATE	9600
+
+
+/* ---- DO NOT TOUCH BELOW THIS LINE ---- */
+
+#if DBG_UART_ENABLE
+
+/**
+ * @brief Debug software UART initialization.
+ */
+#define dbg_tx_init()	do { \
+		DBG_UART_TX_PORT |= (1<<DBG_UART_TX_PIN); \
+		DBG_UART_TX_DDR |= (1<<DBG_UART_TX_PIN); \
+	} while(0)
+
+/**
+ * @brief Software UART routine for transmitting debug information. 
+ *
+ * @warning This routine blocks all interrupts until all 10 bits  ( 1 START +
+ * 8 DATA + 1 STOP) are transmitted. This would be about 1ms with 9600bps.
+ *
+ * @note Calculation for the number of CPU cycles, executed for one bit time:
+ * F_CPU/BAUDRATE = (3*1+2) + (2*1 + (NDLY-1)*4 + 2+1) + 6*1
+ *
+ * (NDLY-1)*4 = F_CPU/BAUDRATE - 16
+ * NDLY = (F_CPU/BAUDRATE-16)/4+1
+ */
+extern void dbg_putchar(uint8_t c);
+
+#else
+  #define dbg_tx_init()		
+  #define dbg_putchar(A)		((void)(A))
+#endif	/* DBG_UART_ENABLE */
+
+#endif	/* DBG_PUTCHAR_H */
+
